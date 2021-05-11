b0f3f0f3bb4e26dc3d7cc3e0c0f00918104f9c80
hannahdoret <83749605+hannahdoret@users.noreply.github.com>
2021-05-11 22:01:45 +0800

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -9,3 +9,17 @@ $("#menu-toggle").click(function (e) {
     e.preventDefault();
     $("#wrapper").toggleClass("toggled");
 });
+
+function openTab(evt, whichTab) {
+    var i, tabContent, tabLinks;
+    tabContent = document.getElementsByClassName("tabcontent");
+    for (i=0; i < tabContent.length; i++){
+        tabContent[i].style.display = "none";
+    }
+    tabLinks = document.getElementsByClassName("tabLinks");
+    for (i=0; i < tabLinks.length; i++){
+        tabLinks[i].className = tabLinks[i].className.replace(" active", "");
+    }
+    document.getElementById(whichTab).style.display = "block";
+    evt.currentTarget.className += " active";
+}
