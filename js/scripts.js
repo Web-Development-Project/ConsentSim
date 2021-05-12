b00092f2098e172ac9324b4301e487b2c4ffffc3
hannahdoret <83749605+hannahdoret@users.noreply.github.com>
2021-05-12 16:36:02 +0800

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -23,3 +23,36 @@ function openTab(evt, whichTab) {
     document.getElementById(whichTab).style.display = "block";
     evt.currentTarget.className += " active";
 }
+
+
+document.getElementById("form1").onsubmit=function() {
+
+    window.location.replace("feedback.html");
+
+    q1 = parseInt(document.querySelector('input[name = "q1"]:checked').value);
+    q2 = parseInt(document.querySelector('input[name = "q2"]:checked').value);
+    q3 = parseInt(document.querySelector('input[name = "q3"]:checked').value);
+    q4 = parseInt(document.querySelector('input[name = "q4"]:checked').value);
+    q5 = parseInt(document.querySelector('input[name = "q5"]:checked').value);
+    q6 = parseInt(document.querySelector('input[name = "q6"]:checked').value);
+    q7 = parseInt(document.querySelector('input[name = "q7"]:checked').value);
+    q8 = parseInt(document.querySelector('input[name = "q8"]:checked').value);
+    q9 = parseInt(document.querySelector('input[name = "q9"]:checked').value);
+    q10 = parseInt(document.querySelector('input[name = "q10"]:checked').value);
+    
+    
+    result = q1 + q2 + q3 + q4 + q5 + q6 + q7 + q8 + q9 + q10;
+    
+
+ //document.getElementById("grade").innerHTML = result;
+
+ 
+
+ 
+    
+
+
+return false; // required to not refresh the page; just leave this here
+} //this ends the submit function
+
+
